ec0efe8effdab44e4dcfee7c1effd1a4922eadcc
Stephen Duffy <duffysd@sunypoly.edu>
2026-09-24 15:34:33 -0400

    hsv stuff

--- SDL3/SDL_HSV/main.cpp
+++ SDL3/SDL_HSV/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SDL3/SDL.h>
 #include <SDL3_image/SDL_image.h>
+#include "color.h"
 
 constexpr SDL_InitFlags initFLags = SDL_INIT_VIDEO;
 
@@ -25,7 +26,6 @@ int main(int argc, char* argv[]) {
 
     int width = image->w;
     int height = image->h;
-    const int imgSize = width * height;
 
     SDL_Window * window = SDL_CreateWindow("SDL Image Window",width,height,SDL_WINDOW_RESIZABLE | SDL_WINDOW_TRANSPARENT);
     if (window == nullptr) {
@@ -35,11 +35,16 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    SDL_Surface * surface = SDL_GetWindowSurface(window);
-    SDL_Surface *image_c = SDL_ConvertSurface(image, surface->format);
-    SDL_DestroySurface(image);
-    image = image_c;
-    auto * imagePixels = static_cast<Uint32*>(image->pixels);
+        SDL_Surface * surface = SDL_GetWindowSurface(window);
+    {
+        SDL_Surface *image_c = SDL_ConvertSurface(image, surface->format);
+        SDL_DestroySurface(image);
+        image = image_c;
+    }
+
+    int pixelsPerRow = static_cast<int>(image->pitch / sizeof(Uint32));
+    auto * pixels = static_cast<Uint32 *>(image->pixels);
+    const SDL_PixelFormatDetails * pixelFormatDetails = SDL_GetPixelFormatDetails(image->format);
 
     //blit stands for BLock Transfer
     SDL_BlitSurface(image, nullptr, surface, nullptr);
@@ -75,7 +80,38 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        //do other render stuff
+        SDL_Color c;
+        float h, s, v;
+        //get to address of pixels
+        Uint32 * nextP;
+        float mouseX, mouseY;
+        SDL_GetMouseState(&mouseX,&mouseY);
+        for (int y = 0; y < image->h; y++) {
+            nextP = pixels + pixelsPerRow * y;
+            for (int x = 0; x < image->w; x++) {
+
+                //use SDL get rgb to retrieve the color of the pixel
+                SDL_GetRGB(*nextP,pixelFormatDetails,nullptr,&c.r,&c.g,&c.b);
+                //convert to HSV
+                convertRGBtoHSV(c.r,c.g,c.b,&h,&s,&v);
+
+                //do hsv modifications
+                h += 10.0f;
+                if (h > 360.0f) {
+                    h -= 360.0f;
+                }
+                v = mouseY / static_cast<float>(image->h);
+                s = mouseX / static_cast<float>(image->w);
+
+                //convert back to RGB
+                convertHSVtoRGB(h,s,v,&c.r,&c.g,&c.b);
+                //update the surface value
+                *nextP = SDL_MapRGB(pixelFormatDetails,nullptr,c.r,c.g,c.b);
+
+                nextP++;
+            }
+        }
+
 
         SDL_BlitSurface(image, nullptr, surface, nullptr);
         SDL_UpdateWindowSurface(window);
